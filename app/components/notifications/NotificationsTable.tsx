864e6a936fe5cab6bd160ab2a77df3594c576c78
SainaFirooz <saina.firooz@knightec.se>
2025-02-27 16:55:37 +0100

    notification chart & table

---
+++ app/components/notifications/NotificationsTable.tsx
@@ -0,0 +1,32 @@
+import { fetchData } from "@/app/utils/count";
+import { NotificationsData } from "./notifications.types";
+
+interface NotificationProps {
+  dateRange: { from: Date | null; to: Date | null };
+}
+
+export async function NotificationTable({ dateRange }: NotificationProps) {
+  const validDateRange = {
+    from: dateRange.from ?? new Date(),
+    to: dateRange.to ?? new Date(),
+  };
+
+  const notificationsData: NotificationsData[] = await fetchData(
+    `${process.env.BACKEND_URL}/api/notifications`,
+    validDateRange
+  );
+
+  return (
+    <div>
+      <h1>Notifications Table</h1>
+      <ul>
+        {notificationsData.map((notifications, index) => (
+          <li key={index}>
+            {notifications.queueUserIds} notifications from:{" "}
+            {notifications.fromDate} to: {notifications.toDate}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
