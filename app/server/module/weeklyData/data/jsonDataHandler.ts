ddb8a96dcbf0b52412cd3001caec7bffa014d42b
SainaFirooz <saina.firooz@knightec.se>
2025-02-27 16:53:53 +0100

    json data

---
+++ app/server/module/weeklyData/data/jsonDataHandler.ts
@@ -0,0 +1,60 @@
+import fs from "fs/promises";
+import path from "path";
+import process from "process";
+import { WeeklyDataType } from "../weeklyData.types";
+
+const dataFilePath = path.resolve(
+  process.cwd(),
+  "app",
+  "server",
+  "module",
+  "weeklyData",
+  "data",
+  "data.json"
+);
+
+async function ensureFileExists() {
+  try {
+    await fs.access(dataFilePath);
+    console.log(dataFilePath);
+  } catch (error) {
+    await fs.writeFile(dataFilePath, JSON.stringify([], null, 2));
+  }
+}
+
+export async function getAllWeeklyData(): Promise<WeeklyDataType[]> {
+  try {
+    const fileData = await fs.readFile(dataFilePath, "utf-8");
+    const jsonData = JSON.parse(fileData) as WeeklyDataType[];
+
+    jsonData.sort(
+      (a, b) => new Date(b.toDate).getTime() - new Date(a.toDate).getTime()
+    );
+
+    return jsonData;
+  } catch (error: any) {
+    console.error("Error reading data.json:", error);
+    return [];
+  }
+}
+
+export async function updateJsonFile(newData: WeeklyDataType) {
+  try {
+    await ensureFileExists();
+
+    const fileData = await fs.readFile(dataFilePath, "utf-8");
+    let jsonData: WeeklyDataType[] = [];
+
+    try {
+      jsonData = JSON.parse(fileData);
+    } catch (parseErr) {
+      console.error("Error parsing data.json:", parseErr);
+    }
+
+    jsonData.push(newData);
+
+    await fs.writeFile(dataFilePath, JSON.stringify(jsonData, null, 2));
+  } catch (error: any) {
+    console.error("Error updating data.json:", error);
+  }
+}
