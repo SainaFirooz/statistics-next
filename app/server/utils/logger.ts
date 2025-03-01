d2a4685f47bcca6dcc0e6a747c6613c242712359
SainaFirooz <saina.firooz@knightec.se>
2025-03-01 14:55:15 +0100

    testing

--- app/server/utils/logger.ts
+++ app/server/utils/logger.ts
@@ -1,12 +1,12 @@
 import pino from "pino";
-const logger = pino({
-  transport: {
-    target: "pino-pretty",
-    options: {
-      colorize: true,
-    },
-  },
-  level: "info",
-});
-
+const logger = pino(
+  process.env.NODE_ENV === "development"
+    ? {
+        transport: {
+          target: "pino-pretty",
+          options: { colorize: true },
+        },
+      }
+    : {}
+);
 export default logger;
