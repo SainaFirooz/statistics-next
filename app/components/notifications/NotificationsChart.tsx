3ab6eb6a2fa102083ddb84db99a1ac103a576cd0
SainaFirooz <saina.firooz@knightec.se>
2025-03-04 19:32:14 +0100

    sorted data, added card and line chart

--- app/components/notifications/NotificationsChart.tsx
+++ app/components/notifications/NotificationsChart.tsx
@@ -1,5 +1,7 @@
 import { ApiResponse, fetchData } from "@/app/utils/count";
 import { NotificationsData } from "./notifications.types";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { LineChartComponent } from "../LineComponent";
 
 interface NotificationProps {
   dateRange: { from: Date | null; to: Date | null };
@@ -19,17 +21,34 @@ export async function NotificationChart({ dateRange }: NotificationProps) {
   if (!response.data || response.data.length === 0) {
     return <div>No data available</div>;
   }
+  const sortedData = [...response.data].sort(
+    (a, b) => new Date(a.fromDate).getTime() - new Date(b.fromDate).getTime()
+  );
+
   return (
     <div>
-      <h1>Notifications Table</h1>
-      <ul>
-        {response.data.map((notifications, index) => (
-          <li key={index}>
-            {notifications.queueUserIds} notifications from:{" "}
-            {notifications.fromDate} to: {notifications.toDate}
-          </li>
-        ))}
-      </ul>
+      <Card>
+        <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+          <CardTitle className="text-sm font-bold">
+            Sent notifications
+          </CardTitle>
+        </CardHeader>
+        <CardContent>
+          <div className="text-h2 font-bold">{sortedData.length}</div>
+          <p className="text-xs text-muted-foreground">last 7 days</p>
+        </CardContent>
+        <div className="h-[60px]">
+          {" "}
+          <LineChartComponent
+            data={sortedData}
+            dataKey={"queueUserIds"}
+            chartConfig={{
+              color: "#00000",
+              label: "Notifications",
+            }}
+          />
+        </div>
+      </Card>
     </div>
   );
 }
