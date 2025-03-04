c24c658adb1d8a9a047b2407a7791e5e59ca3d2f
SainaFirooz <saina.firooz@knightec.se>
2025-03-04 19:33:34 +0100

    created trending down and trending up components

---
+++ app/components/trending/TrendingUp.tsx
@@ -0,0 +1,18 @@
+function TrendingUp() {
+  return (
+    <div className="bg-icon-50 p-2 rounded-lg flex items-center gap-3">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        height="17px"
+        viewBox="0 -960 960 960"
+        width="17px"
+        fill="#047420"
+      >
+        <path d="m136-240-56-56 296-298 160 160 208-206H640v-80h240v240h-80v-104L536-320 376-480 136-240Z" />
+      </svg>
+      <p className="text-cap text-icon-100 font-medium">2,5%</p>
+    </div>
+  );
+}
+
+export default TrendingUp;
