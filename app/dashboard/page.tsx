553971f647009e698dd9b941cd847d2bf6caecbd
SainaFirooz <saina.firooz@knightec.se>
2025-03-11 21:26:21 +0100

    changed dhasboard classnames

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,13 +2,14 @@ import { Suspense } from "react";
 import Loading from "./loading";
 import { UsersChart } from "../components/users/UsersChart";
 import { SubscriptionsChart } from "../components/subscriptions/SubscriptionsChart";
-import { NotificationChart } from "../components/notifications/NotificationsChart";
+import { NotificationChart } from "../components/notifications/TopNotificationsChart";
 import { IncidentMessagesChart } from "../components/incidentMessages/IncidentMessageChart";
 import { WeeklyDataChart } from "../components/weeklyData/WeeklyDataChart";
 import { WeeklyDataTable } from "../components/weeklyData/WeeklyDataTable";
-import { NotificationTable } from "../components/notifications/NotificationsTable";
 import { DatePicker } from "../components/DatePicker";
 import { DateFilter } from "../components/DateFilter";
+import { SideNotificationChart } from "../components/notifications/SideNotificationChart";
+import UsersSideChart from "../components/users/UsersSideChart";
 
 export const dynamic = "force-dynamic";
 
@@ -19,57 +20,59 @@ export default async function DashboardPage() {
   };
 
   return (
-    <div className="grid grid-cols-12 gap-6 p-6">
-      <div className="col-span-12 flex items-center justify-between gap-4">
-        <DatePicker />
-        <div className="ml-auto">
-          <DateFilter />
+    <div className="bg-white max-w-7xl">
+      <div className="grid grid-cols-12 gap-6 p-6 bg-white dark:bg-grey-900">
+        <div className="col-span-12 flex items-center justify-between gap-4">
+          <DatePicker />
+          <div className="ml-auto">
+            <DateFilter />
+          </div>
         </div>
-      </div>
-      <div className="col-span-12 grid grid-cols-12 gap-6">
-        <div className="col-span-3">
+        <div className="col-span-12 grid grid-cols-12 gap-6">
+          <div className="col-span-3">
+            <Suspense fallback={<Loading />}>
+              <UsersChart dateRange={dateRange} />
+            </Suspense>
+          </div>
+          <div className="col-span-3">
+            <Suspense fallback={<Loading />}>
+              <IncidentMessagesChart dateRange={dateRange} />
+            </Suspense>
+          </div>
+          <div className="col-span-3">
+            <Suspense fallback={<Loading />}>
+              <SubscriptionsChart dateRange={dateRange} />
+            </Suspense>
+          </div>
+          <div className="col-span-3">
+            <Suspense fallback={<Loading />}>
+              <NotificationChart dateRange={dateRange} />
+            </Suspense>
+          </div>
+        </div>
+        <div className="col-span-12">
           <Suspense fallback={<Loading />}>
-            <UsersChart dateRange={dateRange} />
+            <WeeklyDataChart dateRange={dateRange} />
           </Suspense>
         </div>
-        <div className="col-span-3">
+        <div className="col-span-6">
           <Suspense fallback={<Loading />}>
-            <IncidentMessagesChart dateRange={dateRange} />
+            <WeeklyDataTable dateRange={dateRange} />
           </Suspense>
         </div>
-        <div className="col-span-3">
-          <Suspense fallback={<Loading />}>
-            <SubscriptionsChart dateRange={dateRange} />
-          </Suspense>
-        </div>
-        <div className="col-span-3">
-          <Suspense fallback={<Loading />}>
-            <NotificationChart dateRange={dateRange} />
-          </Suspense>
-        </div>
-      </div>
-      <div className="col-span-12">
-        <Suspense fallback={<Loading />}>
-          <WeeklyDataChart dateRange={dateRange} />
-        </Suspense>
-      </div>
-      <div className="col-span-6">
-        <Suspense fallback={<Loading />}>
-          <WeeklyDataTable dateRange={dateRange} />
-        </Suspense>
-      </div>
-      <div className="col-span-6 grid grid-rows-2 gap-4">
-        <div className="row-span-1">
-          <Suspense fallback={<Loading />}>
-            <NotificationTable dateRange={dateRange} />
-          </Suspense>
+        <div className="col-span-6 grid grid-rows-2 gap-4">
+          <div className="row-span-1">
+            <Suspense fallback={<Loading />}>
+              <SideNotificationChart dateRange={dateRange} />
+            </Suspense>
+          </div>
+          <div className="row-span-1">
+            <Suspense fallback={<Loading />}>
+              <UsersSideChart dateRange={dateRange} />
+            </Suspense>
+          </div>
         </div>
       </div>
-      {/* <div className="col-span-6">
-        <Suspense fallback={<Loading />}>
-          <UsersTable dateRange={dateRange} />
-        </Suspense>
-      </div> */}
     </div>
   );
 }
