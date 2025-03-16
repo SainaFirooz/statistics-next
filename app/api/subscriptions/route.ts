8ed322231207e4aed12b5edf48677998bdbff1b3
SainaFirooz <saina.firooz@knightec.se>
2025-03-16 14:45:44 +0100

    deleted routes

--- app/api/subscriptions/route.ts
+++
@@ -1,3 +0,0 @@
-import { GET } from "@/app/server/module/subscriptions/subscriptions.controller";
-
-export { GET };
--- app/api/users/route.ts
+++
@@ -1,3 +0,0 @@
-import { GET } from "@/app/server/module/users/users.controller";
-
-export { GET };
