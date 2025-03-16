0952a36c076e50b534d6311524e8e14db3b47a7c
SainaFirooz <saina.firooz@knightec.se>
2025-03-16 14:52:51 +0100

    dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,44 +1,53 @@
 import { Suspense } from "react";
 import Loading from "./loading";
-import { TopChart, UsersChart } from "../components/users/UsersChart";
-import { SubscriptionsChart } from "../components/subscriptions/SubscriptionsChart";
-import { NotificationChart } from "../components/notifications/TopNotificationsChart";
-import { IncidentMessagesChart } from "../components/incidentMessages/IncidentMessageChart";
+import { TopChart } from "../components/topChart/TopChart";
 import { WeeklyDataChart } from "../components/weeklyData/WeeklyDataChart";
 import { WeeklyDataTable } from "../components/weeklyData/WeeklyDataTable";
-import { SideNotificationChart } from "../components/notifications/SideNotificationChart";
-import UsersSideChart from "../components/users/UsersSideChart";
-import DateComponent from "../components/DateComponent";
+import { SideNotificationChart } from "../components/notification/SideNotificationChart";
+import UsersSideChart from "../components/notification/NotificationVsIncident";
 import { DateRange } from "react-day-picker";
-import { fetchData } from "../utils/count";
-import { UserData } from "../components/users/users.types";
+import { ApiResponse, fetchData } from "../utils/fetchData";
+import { TopChartDataInput } from "../components/topChart/users.types";
+import { AllData } from "../global.types";
+import { WeeklyData } from "../components/weeklyData/weeklyData.types";
+import DateComponent from "../components/date/DateComponent";
 
 export const dynamic = "force-dynamic";
 
 export default async function DashboardPage({
   searchParams,
 }: {
-  searchParams: { startDate?: string; endDate?: string };
+  searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
 }) {
+  const params = await searchParams;
+  const startDateParam =
+    typeof params.startDate === "string" ? params.startDate : "2024-01-01";
+
+  const endDateParam =
+    typeof params.endDate === "string"
+      ? params.endDate
+      : new Date().toISOString();
+
+  const dateRange: DateRange = {
+    from: dateHelper(startDateParam),
+    to: dateHelper(endDateParam),
+  };
   function dateHelper(date: string) {
     return new Date(date);
   }
-  const dateRange: DateRange = {
-    from: dateHelper(searchParams.startDate ?? "2025-03-01"),
-    to: dateHelper(searchParams.endDate ?? "2025-03-01"),
-  };
 
-  const [data1] = await Promise.all([
-    fetchData<ApiResponse<UserData[]>>(
-      `${process.env.BACKEND_URL}/api/users`,
-      dateRange
-    ),
+  const data: ApiResponse<AllData> = await fetchData<AllData>(
+    `${process.env.BACKEND_URL}/api/all`,
+    dateRange
+  );
 
-    // ...other API calls
-  ]);
+  const weeklyData: ApiResponse<WeeklyData[]> = await fetchData<WeeklyData[]>(
+    `${process.env.BACKEND_URL}/api/weeklyData`,
+    dateRange
+  );
 
   return (
-    <div className="bg-white dark:bg-grey-900 max-w-7xl max-h-svw ">
+    <div className="bg-white dark:bg-grey-900 max-w-7xl max-h-svw pb-6">
       <DateComponent />
       <div className="grid grid-cols-4 gap-6">
         <Suspense fallback={<Loading />}>
@@ -46,39 +55,68 @@ export default async function DashboardPage({
             dateRange={dateRange}
             title="Users"
             lineColor="#F6A600"
-            data={data1}
+            data={data.success ? (data.data.users as TopChartDataInput[]) : []}
+            percentageChange={0}
           />
         </Suspense>
         <Suspense fallback={<Loading />}>
-          <IncidentMessagesChart dateRange={dateRange} />
+          <TopChart
+            dateRange={dateRange}
+            title="Incidents"
+            lineColor="#015AAA"
+            data={
+              data.success ? (data.data.incidents as TopChartDataInput[]) : []
+            }
+            percentageChange={0}
+          />
         </Suspense>
         <Suspense fallback={<Loading />}>
-          <SubscriptionsChart dateRange={dateRange} />
+          <TopChart
+            dateRange={dateRange}
+            title="Subscriptions"
+            lineColor="#4A806A"
+            data={
+              data.success
+                ? (data.data.subscriptions as TopChartDataInput[])
+                : []
+            }
+            percentageChange={0}
+          />
         </Suspense>
         <Suspense fallback={<Loading />}>
-          <NotificationChart dateRange={dateRange} />
+          <TopChart
+            dateRange={dateRange}
+            title="Push notifications"
+            lineColor="#3079DB"
+            data={
+              data.success
+                ? (data.data.notifications as TopChartDataInput[])
+                : []
+            }
+            percentageChange={0}
+          />
         </Suspense>
       </div>
       <div className="mt-6">
         <Suspense fallback={<Loading />}>
-          <WeeklyDataChart dateRange={dateRange} />
+          <WeeklyDataChart data={weeklyData} />
         </Suspense>
       </div>
       <div className="grid grid-cols-12 gap-6 mt-6">
         <div className="col-span-7">
           <Suspense fallback={<Loading />}>
-            <WeeklyDataTable dateRange={dateRange} />
+            <WeeklyDataTable data={weeklyData} />
           </Suspense>
         </div>
-        <div className="col-span-5 grid grid-rows-1 gap-6">
+        <div className="col-span-5 grid grid-rows-1">
           <div className="row-span-1">
             <Suspense fallback={<Loading />}>
-              <SideNotificationChart dateRange={dateRange} />
+              <SideNotificationChart data={data} />
             </Suspense>
           </div>
           <div className="row-span-1">
             <Suspense fallback={<Loading />}>
-              <UsersSideChart dateRange={dateRange} />
+              <UsersSideChart data={data} />
             </Suspense>
           </div>
         </div>
