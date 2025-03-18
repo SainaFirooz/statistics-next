56e0c4acf64cde8e686b767c4b091ee6a64c6638
SainaFirooz <saina.firooz@knightec.se>
2025-03-18 20:52:20 +0100

    used credentials provider to use email and password stored in env file

---
+++ app/api/auth/authOptions.ts
@@ -0,0 +1,29 @@
+import CredentialsProvider from "next-auth/providers/credentials";
+
+export const authOptions = {
+  secret: process.env.NEXTAUTH_SECRET,
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: {
+          label: "Email",
+          type: "email",
+        },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials) {
+        if (
+          credentials?.email === process.env.ADMIN_EMAIL &&
+          credentials?.password === process.env.ADMIN_PASSWORD
+        ) {
+          return { id: "1", name: "Admin", email: process.env.ADMIN_EMAIL };
+        }
+        throw new Error("Invalid email or password");
+      },
+    }),
+  ],
+  pages: {
+    signIn: "/login",
+  },
+};
