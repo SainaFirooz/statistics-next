2166de1b6eb5839a9f6c7d89aea5926534c242c2
SainaFirooz <saina.firooz@knightec.se>
2025-03-18 20:51:47 +0100

    added auth provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Montserrat } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "./components/theme-provider";
 import Header from "./components/Header";
+import AuthProvider from "./AuthProvider";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -21,7 +22,7 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <body
         className={`${montserrat.className} dark:bg-grey-900 bg-white
-`}
+          `}
       >
         <ThemeProvider
           attribute="class"
@@ -29,10 +30,13 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
-          <Header />
-          <main className="flex justify-center justify-items-center dark:bg-grey-900 mt-16">
-            {children}
-          </main>
+          <AuthProvider>
+            <Header />
+
+            <main className="flex justify-center justify-items-center dark:bg-grey-900 mt-16">
+              {children}
+            </main>
+          </AuthProvider>
         </ThemeProvider>
       </body>
     </html>
