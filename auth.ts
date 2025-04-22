1733fbe72d59cf69a1f7023cf432de8bf5d1fbdb
SainaFirooz <saina.firooz@knightec.se>
2025-04-22 15:30:26 +0200

    fixed issues with next auth and session

---
+++ auth.ts
@@ -0,0 +1,31 @@
+import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+export const { auth, handlers, signIn, signOut } = NextAuth({
+  secret: process.env.NEXTAUTH_SECRET,
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: { label: "Email", type: "email" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials) {
+        if (
+          credentials?.email === process.env.ADMIN_EMAIL &&
+          credentials?.password === process.env.ADMIN_PASSWORD
+        ) {
+          return {
+            id: "1",
+            name: "Admin",
+            email: process.env.ADMIN_EMAIL,
+          };
+        }
+        throw new Error("Invalid credentials");
+      },
+    }),
+  ],
+  pages: {
+    signIn: "/login",
+  },
+});
